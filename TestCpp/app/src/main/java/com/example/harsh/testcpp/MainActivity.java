1011de6adadaf255949252d2c93544ca092bdcba
harsh <harshvs@live.com>
2017-08-14 06:27:32 +0530

    Clean up

--- TestCpp/app/src/main/java/com/example/harsh/testcpp/MainActivity.java
+++ TestCpp/app/src/main/java/com/example/harsh/testcpp/MainActivity.java
@@ -13,26 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-
-        Button btn = (Button) findViewById(R.id.runTest);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // GoogleTestDriver.scheduleGTests();
-            }
-        });
-
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 
     // Used to load the 'native-lib' library on application startup.
     static {
